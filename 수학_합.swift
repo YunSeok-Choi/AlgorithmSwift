6efa83c36a5007c2a9a1c31bef1b9d4a6c5feab8
YunSeok-Choi <dbstjr9898@naver.com>
2022-04-12 00:49:42 +0900

    8393

---
+++ 수학_합.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  AlgorithmSwift
+//
+//  Created by 최윤석 on 2022/04/12.
+//
+
+import Foundation
+
+var n = Int(readLine()!)!
+var sum = 0
+for i in 1...n{
+    sum += i
+}
+print(sum)
