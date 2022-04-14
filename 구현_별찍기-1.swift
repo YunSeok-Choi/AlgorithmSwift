1c331dc8443ed3f93be68f1edac720df51f36977
YunSeok-Choi <dbstjr9898@naver.com>
2022-04-14 23:21:37 +0900

    2438

---
+++ 구현_별찍기-1.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  AlgorithmSwift
+//
+//  Created by 최윤석 on 2022/04/14.
+//
+
+import Foundation
+
+var n = Int(readLine()!)!
+for i in 1...n{
+    for _ in 0..<i{
+        print("*", terminator: "")
+    }
+    print()
+}
